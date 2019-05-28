7fc7725a94602b85fb7201fa9dc4f675c690ac55
hemachandar112358 <48442501+hemachandar112358@users.noreply.github.com>
2019-05-28 08:41:37 +0530

    Update test.py

--- test.py
+++ test.py
@@ -13,14 +13,13 @@ links = ['https://duckduckgo.com','https://google.com'];
 
 now=datetime.now()
 filename=now.strftime("%m%d%Y%H%M%S")
-print(filename)
-#print(now.split('.')[0].replace(':','').strip())
+
 
 doc=docx.Document()
-filepath = 'C:\\Users\\Hemachandar\\Downloads\\tdr'+filename+'.docx' 
+filepath = 'C:\\Users\\username\\Downloads\\tdr'+filename+'.docx' 
 doc.save(filepath)
 
-browser=webdriver.Chrome(executable_path='C:/Users/Hemachandar/Downloads/chromedriver.exe');
+browser=webdriver.Chrome(executable_path='C:/Users/username/Downloads/chromedriver.exe');
 browser.maximize_window()
 
 for x in links:
@@ -29,7 +28,7 @@ for x in links:
     browser.get(x)
     filename='C:\\Users\\Hemachandar\\Downloads\\' + x.split('//')[1].split('.')[0] + '_screenshot.png'
     browser.save_screenshot("C:\\Users\\Hemachandar\\Downloads\\" + x.split('//')[1].split('.')[0] + "_screenshot.png")
-    #screenshot = browser.get_screenshot_as_png();
+    
     doc.add_picture(filename,width=docx.shared.Inches(7),height=docx.shared.Inches(11))
     doc.save(filepath)
     
